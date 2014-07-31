59274982c0fb6ebdf30cdf43c628ca3e31c191d9
Mark Miyashita <negativetwelve@gmail.com>
2014-07-31 09:07:48 -0700

    test

--- git.py
+++ git.py
@@ -4,7 +4,8 @@ import subprocess
 
 def git(space_delimited_args):
     print "Running git %s" % space_delimited_args
-    return subprocess.Popen("git " + space_delimited_args)
+    list_of_args = space_delimited_args.split()
+    return subprocess.Popen(["git"]  + list_of_args)
 
 def git_commit(message):
     return git("commit -am " + message)
